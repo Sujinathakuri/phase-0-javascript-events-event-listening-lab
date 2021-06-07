021da104d8f1507762ccb67db08363b2e258fcf1
Sujinathakuri <sujchand2@gmail.com>
2021-06-07 16:42:22 -0400

    commit tree 55e239f3dc602186ecfa91c68c238b198eea0858

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-
-    function clickAlert() {
-        alert('I was clicked!');
-    }
-
-    input.addEventListener('click', clickAlert);
-}
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
